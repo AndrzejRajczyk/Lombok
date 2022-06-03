260957bc374217519415c71a0bc190d6d87f372a
AndrzejRajczyk <rajczyk.andrzej@gmail.com>
2022-06-03 12:53:23 +0200

    module19

---
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -0,0 +1,54 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/tasks")
+@RequiredArgsConstructor
+public class TaskController {
+@Autowired
+    private final DbService service;
+@Autowired
+    private final TaskMapper taskMapper;
+
+
+    @GetMapping
+    public List<TaskDto> getTasks(){
+
+      List<Task> tasks = service.getAllTasks();
+      return taskMapper.mapToTaskDtoList(tasks);
+  }
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void createTask(@RequestBody TaskDto taskDto) {
+        Task task = taskMapper.mapToTask(taskDto);
+        service.saveTask(task);
+    }
+    @GetMapping(value = {"taskId"})
+    public ResponseEntity<TaskDto> getTask(@PathVariable Long taskId)throws TaskNotFoundException {
+
+
+        return new ResponseEntity<>(taskMapper.mapToTaskDto(
+                service.getTask(taskId)), HttpStatus.OK);
+
+    }
+    @DeleteMapping(value = "taskId")
+    public void deleteTask( Long id) {
+        service.deleteTask(id);
+
+    }
+
+}
+
+            
+
--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -5,18 +5,37 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name="tasks")
 public class Task {
+    @Id
+    @GeneratedValue
     private Long id;
+   @Column(name ="name")
     private String title;
+   @Column(name="description")
     private String content;
 
 
-    git add README.md
     public Long getId() {
         return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
     }
 }
